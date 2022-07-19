7048860f6d33d4020590193af625da6664172a10
Chris Blackburn <chrisnblackburn@gmail.com>
2022-07-18 20:46:18 -0400

    move to cmake, add sdl2

--- src/poboy.c
+++ src/poboy.c
@@ -2,11 +2,35 @@
 #include <stdint.h>
 
 #include <poboy.h>
+#include <SDL.h>
 
 struct poboy ctx = { 0 };
 
-int main(void) {
-    printf("Hello, World! %p (%zu)\n", (void*)&ctx, sizeof(ctx));
+int main(int argc, char *argv[]) {
+    (void)argc;
+    (void)argv;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow("poboy",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            640,
+            480,
+            0);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window,
+            -1,
+            SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
 
